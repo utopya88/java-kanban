2693c3db43495ec7068e5b54ca34d654a562c139
Dmitry Savchenko <starmebest@mail.ru>
2024-08-07 16:51:39 +0300

    tests

--- src/test/HistoryManagerTest.java
+++ src/test/HistoryManagerTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 
 class HistoryManagerTest {
     HistoryManager historyManager;
+
     @BeforeEach
     void beforeEach() {
         historyManager = Managers.getDefaultHistory();
