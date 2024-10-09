0cfd7c08a83da328b5968473531f64326cd14dbf
Dmitry Savchenko <starmebest@mail.ru>
2024-10-09 15:55:47 +0300

    s

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -13,11 +13,7 @@ public class Managers {
     public static TaskManager getDefaultInMemoryManager() {
         return new InMemoryTaskManager(getDefaultHistory());
     }
-
-    public static TaskManager getDefault() {
-        return new HttpTaskManager("http://localhost:8078");
-    }
-
+    
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
